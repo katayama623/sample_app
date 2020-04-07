7cf29f107134cd0112a1897031cca8615ebec4e0
片山優一郎 <k6t2y3m@gmail.com>
2020-04-07 09:15:54 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7b8fdf5d9a905f2e56b9fc33ea08f048c690f3a91c5e14a08e5526073f8ae0a442799eb2a9f477d3e1288bd515d19de1f5e21fb91efd1df54d6262cb03cb3450'
+
